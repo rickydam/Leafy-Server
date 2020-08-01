e04255f65a0d6dd6e89e90edda449f9d54805588
Ricky Dam <RickyDamOttawa@gmail.com>
2020-08-01 16:18:44 -0400

    Upgrade Ruby properly (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_731_143_013) do
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'username'
-    t.string 'password_digest'
+ActiveRecord::Schema.define(version: 2020_07_31_143013) do
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password_digest"
   end
+
 end
