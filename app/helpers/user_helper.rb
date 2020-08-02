a0c21118659eeaea367493ee1fee6a77eb8b31d3
Ricky Dam <RickyDamOttawa@gmail.com>
2020-08-01 22:54:51 -0400

    Switch to api project (#6)

--- app/helpers/user_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# A helper for the User model
-module UserHelper
-end
--- db/migrate/20200731143013_add_authentication_to_users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Add required columns for bcrypt
-class AddAuthenticationToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :password_digest, :string
-  end
-end
